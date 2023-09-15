b0def94689ae40deeb9ab5c8084f291e3a0546c2
Christian Alexander Ledaal <40630712+Zekima@users.noreply.github.com>
2023-09-15 20:48:11 +0200

    Added PriceData data class

---
+++ app/src/main/java/com/example/stromprisapp/PriceData.kt
@@ -0,0 +1,9 @@
+package com.example.stromprisapp
+
+data class PriceData(
+    val NOK_per_kWh: Double,
+    val EUR_per_kWh: Double,
+    val EXR: Double,
+    val time_start: String,
+    val time_end: String
+)
