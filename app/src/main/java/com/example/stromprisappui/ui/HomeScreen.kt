e329deede4dcaac6ac7a300e6c5396fb52b196cc
Magnus <40308032+magnuslf1606@users.noreply.github.com>
2023-09-29 18:30:38 +0200

    Update HomeScreen.kt
    UI for homescreen

--- app/src/main/java/com/example/stromprisappui/ui/HomeScreen.kt
+++ app/src/main/java/com/example/stromprisappui/ui/HomeScreen.kt
@@ -1,9 +1,86 @@
-package com.example.stromprisappui.ui
+package com.example.stromprisapp.ui
 
-import androidx.compose.material3.Text
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.util.Date
 
+@RequiresApi(Build.VERSION_CODES.O)
+@SuppressLint("SimpleDateFormat")
+@Preview
 @Composable
 fun HomeScreen() {
-    Text("Hello from Home")
+    val litenOverskrift = 20.sp; val pris = 50.sp; val valuta = 17.sp
+    val datesize = 16.sp; val paddingMellomOverskrifter = 70.dp
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        TekstMedBakgrunn(
+            backgroundColor = Global.bakgrunnsfarge,
+            "Strømpriser",
+            modifier = Modifier.padding(top = paddingMellomOverskrifter),
+            fontSize = 50.sp
+        )
+        TekstMedBakgrunn(
+            backgroundColor = Global.bakgrunnsfarge,
+            "Strømprisen idag",
+            modifier = Modifier.padding(top = paddingMellomOverskrifter),
+            fontSize = litenOverskrift,
+            fontWeight = FontWeight.Bold
+        )
+        TekstMedBakgrunn(
+            backgroundColor = Global.bakgrunnsfarge,
+            ""+ SimpleDateFormat("dd/MM/yyyy - hh:mm z").format(Date.from(Instant.now())),
+            fontSize = datesize
+        )
+        Row() {
+            TekstMedBakgrunn(
+                backgroundColor = Global.bakgrunnsfarge,
+                "143.5",
+                fontSize = pris
+
+            )
+            TekstMedBakgrunn(
+                backgroundColor = Global.bakgrunnsfarge,
+                "øre/kWh",
+                modifier = Modifier.padding(top = 32.dp),
+                fontSize = valuta
+            )
+        }
+        TekstMedBakgrunn(
+            backgroundColor = Global.bakgrunnsfarge,
+            "Medianpris for gårsdagen",
+            modifier = Modifier.padding(top = paddingMellomOverskrifter),
+            fontSize = litenOverskrift,
+            fontWeight = FontWeight.Bold
+        )
+        Row() {
+            TekstMedBakgrunn(
+                backgroundColor = Global.bakgrunnsfarge,
+                "139.7",
+                fontSize = pris
+            )
+            TekstMedBakgrunn(
+                backgroundColor = Global.bakgrunnsfarge,
+                "øre/kWh",
+                modifier = Modifier.padding(top = 32.dp),
+                fontSize = valuta
+            )
+        }
+    }
 }
