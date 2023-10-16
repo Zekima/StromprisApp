964c458ce5a81b1eaea14ec266778bbd41c0247b
alexoviflo <alexander.ovesen@online.no>
2023-10-16 14:30:29 +0200

    Update Theme.kt

--- app/src/main/java/com/example/stromprisapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/stromprisapp/ui/theme/Theme.kt
@@ -14,6 +14,11 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -57,6 +62,8 @@ fun StromprisAppTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
+            val rootview = window.decorView
+            rootview.setBackgroundColor(if (darkTheme) Black.toArgb() else White.toArgb())
             window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
