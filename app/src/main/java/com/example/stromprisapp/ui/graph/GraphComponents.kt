85cbfd31a6e082434ee531ab4e7756202de3d1e4
Christian Alexander Ledaal <40630712+Zekima@users.noreply.github.com>
2023-10-24 11:14:26 +0200

    yAxis labels and separated GraphContent from GraphScreen

--- app/src/main/java/com/example/stromprisapp/ui/graph/GraphComponents.kt
+++ app/src/main/java/com/example/stromprisapp/ui/graph/GraphComponents.kt
@@ -3,10 +3,13 @@ package com.example.stromprisapp.ui.graph
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,18 +19,34 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.stromprisapp.PriceData
 import com.example.stromprisapp.Utils
+import com.example.stromprisapp.Utils.fetchApiData
+import com.example.stromprisapp.Utils.includeFees
+import com.example.stromprisapp.ui.Global.valgtSone
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @Composable
 fun RegionRow(navController: NavController) {
@@ -86,9 +105,11 @@ fun PriceTooltip(
 ) {
     if (selectedDataPoint != null) {
 
+        // eur/nok revamp trengs
+
         val orePerKwhValue = selectedDataPoint.nokPerKwh * 100
         val displayPrice = if (includeFees) {
-            val feesIncludedValue = Utils.includeFees(orePerKwhValue)
+            val feesIncludedValue = Utils.includeFees(orePerKwhValue, "NOK")
             if (feesIncludedValue >= 100) String.format("%.0f", feesIncludedValue)
             else String.format("%.2f", feesIncludedValue)
         } else {
@@ -136,3 +157,203 @@ fun PriceTooltip(
         }
     }
 }
+
+@Composable
+fun FetchPriceData(selectedDate: String, onResult: (List<PriceData>?) -> Unit) {
+    val fetchResult = remember { mutableStateOf<List<PriceData>?>(null) }
+
+    LaunchedEffect(selectedDate) {
+        try {
+            val (year, month, day) = selectedDate.split("-")
+            val result = withContext(Dispatchers.IO) {
+                fetchApiData(year, month, day, valgtSone)
+            }
+            fetchResult.value = result
+        } finally {
+            onResult(fetchResult.value)
+        }
+    }
+}
+
+@Composable
+fun GraphContent(
+    sortedData: List<PriceData>?,
+    selectedDataPoint: PriceData?,
+    onSelectedDataPointChanged: (PriceData?) -> Unit,
+    selectedDataPointIndex: Int,
+    onSelectedDataPointIndexChanged: (Int) -> Unit,
+    isLoading: Boolean,
+    checked: Boolean
+) {
+    val hitboxRecs = remember { mutableStateOf(ArrayList<Rect>()) }
+    val pointRecs = remember { mutableStateOf(ArrayList<Rect>()) }
+
+    var path: Path? = null
+
+    val boxSize = remember { mutableStateOf(Size(0f, 0f)) }
+
+    var xScale = 0f
+    var yScale = 0f
+    var minPrice = 0f
+    var maxPrice: Float
+
+    Box(
+        modifier = Modifier
+            .aspectRatio(3 / 2f)
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onTap = { tapOffset ->
+                        hitboxRecs.value.forEachIndexed { index, rect ->
+                            if (rect.contains(tapOffset) && sortedData != null) {
+                                onSelectedDataPointChanged(sortedData[index])
+                                onSelectedDataPointIndexChanged(index)
+                            }
+                        }
+                    }
+                )
+            }
+            .drawWithCache {
+
+                boxSize.value = size
+                var yAxisLabelCount = 0;
+                var yAxisInterval = 0f;
+
+
+                // eur/nok revamp
+
+                if (sortedData != null) {
+                    maxPrice = sortedData
+                        .maxOf { it.nokPerKwh }
+                        .toFloat()
+                    minPrice = sortedData
+                        .minOf { it.nokPerKwh }
+                        .toFloat()
+
+                    val paddingPercentage = 0.25f
+                    val availableHeight = (1 - paddingPercentage) * boxSize.value.height
+
+                    yScale = availableHeight / (maxPrice - minPrice)
+                    xScale = boxSize.value.width / (sortedData.size - 1)
+
+                    path = generatePath(sortedData, xScale, yScale, boxSize.value)
+
+                    yAxisLabelCount = 4
+                    yAxisInterval = (maxPrice - minPrice) / (yAxisLabelCount - 1)
+                }
+
+                onDrawBehind {
+                    drawGrid(boxSize.value)
+                    if (path != null) {
+                        drawPath(path!!, Color.Gray, style = Stroke(4.dp.toPx()))
+                    }
+
+                    val paint = android.graphics
+                        .Paint()
+                        .apply {
+                            color = android.graphics.Color.BLACK
+                            textSize = 29f
+                            textAlign = android.graphics.Paint.Align.RIGHT
+                        }
+
+                    for (i in 0 until yAxisLabelCount) {
+
+                        // eur/nok revamp
+
+                        val priceLabelValue = if (checked) {
+                            includeFees(
+                                (minPrice + i * yAxisInterval).toDouble() * 100,
+                                "NOK"
+                            ).toFloat()
+                        } else {
+                            (minPrice + i * yAxisInterval) * 100
+                        }
+
+                        val priceLabel = String.format("%.0f", priceLabelValue)
+
+                        val y =
+                            boxSize.value.height - ((minPrice + i * yAxisInterval) - minPrice) * yScale - 50f
+
+                        drawContext.canvas.nativeCanvas.drawText(
+                            priceLabel,
+                            0f,
+                            y,
+                            paint
+                        )
+                    }
+
+                    sortedData?.forEachIndexed { i, priceData ->
+                        val x = i * xScale
+                        val y = boxSize.value.height + 40
+
+                        val timeLabel = Utils.convertTime(priceData.timeStart, "HH")
+
+                        drawContext.canvas.nativeCanvas.drawText(
+                            timeLabel,
+                            x,
+                            y,
+                            paint
+                        )
+                    }
+
+                    hitboxRecs.value.clear()
+                    pointRecs.value.clear()
+
+                    val circleRadius = 4.dp.toPx()
+                    sortedData?.forEachIndexed { i, priceData ->
+                        val x = i * xScale
+                        val y =
+                            (boxSize.value.height - (priceData.nokPerKwh - minPrice) * yScale - bottomOffset).toFloat()
+
+                        drawCircle(
+                            Color.DarkGray,
+                            radius = circleRadius,
+                            center = Offset(x, y)
+                        )
+
+                        val pointRect = Rect(
+                            top = y - circleRadius,
+                            left = x - circleRadius,
+                            bottom = y + circleRadius,
+                            right = x + circleRadius
+                        )
+
+                        pointRecs.value.add(pointRect)
+
+                        val hitboxRect = Rect(
+                            top = 0f,
+                            left = x - (circleRadius + 9),
+                            bottom = boxSize.value.height,
+                            right = x + (circleRadius + 9)
+                        )
+
+                        hitboxRecs.value.add(hitboxRect)
+                    }
+                }
+            }
+    ) {
+
+        // legg til auto tooltip popup for idag
+
+//        if (shouldUpdateDataPoint && activeButton === "today" && sortedData !== null && selectedDataPoint === null) {
+//            if ((selectedHour) < sortedData.size) {
+//                selectedDataPoint = sortedData[selectedHour]
+//                selectedDataPointIndex = selectedHour
+//            } else {
+//                selectedDataPoint = null
+//            }
+//            shouldUpdateDataPoint = false
+//        }
+
+        if (!isLoading && selectedDataPoint !== null && selectedDataPointIndex < pointRecs.value.size) {
+            val currentRect = pointRecs.value[selectedDataPointIndex]
+            PriceTooltip(
+                selectedDataPoint = selectedDataPoint,
+                rect = currentRect,
+                includeFees = checked
+            )
+        }
+    }
+}
+
+
--- app/src/main/java/com/example/stromprisapp/ui/graph/GraphScreen.kt
+++ app/src/main/java/com/example/stromprisapp/ui/graph/GraphScreen.kt
@@ -1,17 +1,11 @@
 package com.example.stromprisapp.ui.graph
 
-import android.util.Log
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -19,30 +13,32 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.stromprisapp.PriceData
 import com.example.stromprisapp.Utils.convertTime
-import com.example.stromprisapp.Utils.fetchApiData
-import com.example.stromprisapp.ui.Global.valgtSone
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 
 const val bottomOffset = 50f;
+
+/*
+
+todo:
+   EUR/NOK tilpasning (trenger ikke å skifte selve path/datapoins siden det er relativ)
+   Automatisk tooltip for idag og klokkeslett
+   Darkmode/light mode tilpasning
+
+
+*/
 
 @Composable
 fun GraphScreen(navController: NavController) {
@@ -59,10 +55,8 @@ fun GraphScreen(navController: NavController) {
     var selectedDataPointIndex by remember { mutableIntStateOf(0) }
     var boxSize by remember { mutableStateOf(Size(0f, 0f)) }
 
-    var scaleState by remember { mutableStateOf(Pair(0f, 0f)) }
 
     var selectedDate by remember { mutableStateOf(currentDate) }
-    var dataChanged by remember { mutableStateOf(false) }
 
     var activeButton by remember { mutableStateOf("today") }
 
@@ -70,6 +64,8 @@ fun GraphScreen(navController: NavController) {
 
     var shouldUpdateDataPoint by remember { mutableStateOf(true) }
     var showTooltip by remember { mutableStateOf(false) }
+
+    var sortedData by remember { mutableStateOf<List<PriceData>?>(null) }
 
     Column(modifier = Modifier.padding(top = 100.dp, start = 16.dp, end = 16.dp)) {
 
@@ -82,7 +78,6 @@ fun GraphScreen(navController: NavController) {
                 selectedDataPoint = null
                 selectedDate = currentDate
                 activeButton = "today"
-                dataChanged = false
                 shouldUpdateDataPoint = true
             },
             onSelectTomorrow = {
@@ -92,184 +87,39 @@ fun GraphScreen(navController: NavController) {
                 calendar.add(Calendar.DAY_OF_YEAR, 1)
                 selectedDate = dateFormat.format(calendar.time)
                 activeButton = "tomorrow"
-                dataChanged = false
                 shouldUpdateDataPoint = true
             }
         )
 
-        val fetchResult = remember { mutableStateOf<List<PriceData>?>(null) }
-
-        LaunchedEffect(selectedDate) {
-            isLoading = true
-            try {
-                val (year, month, day) = selectedDate.split("-")
-                val result = withContext(Dispatchers.IO) {
-                    fetchApiData(year, month, day, valgtSone)
-                }
-                fetchResult.value = result
-                dataChanged = true
-            } finally {
-                isLoading = false
-            }
-        }
-
-        LaunchedEffect(fetchResult.value) {
-            if (fetchResult.value != null) {
-                isLoading = false
-            }
-        }
-
-        var xScale = 0f
-        var yScale = 0f
-        var minPrice = 0f
-        var maxPrice: Float
-
-        val hitboxRecs = remember { mutableStateOf(ArrayList<Rect>()) }
-        val pointRecs = remember { mutableStateOf(ArrayList<Rect>()) }
         var checked by remember { mutableStateOf(true) }
 
-        // Sorter kun når fetchResult endrer seg
-        var sortedData = remember(fetchResult.value) {
-            fetchResult.value?.sortedBy { convertTime(it.timeStart, "HH") }
+        FetchPriceData(selectedDate) { result ->
+            if (result != null) {
+                sortedData = result.sortedBy { convertTime(it.timeStart, "HH") }
+            } else {
+                sortedData = null
+            }
         }
 
         if (sortedData == null) {
-            return;
+            Text(text = "Strømpriser for i morgen publiseres kl 13:00 i dag")
+            return
         }
 
-        Box(
-            modifier = Modifier
-                .aspectRatio(3 / 2f)
-                .fillMaxSize()
-                .pointerInput(Unit) {
-                    val sortedDataLocal = sortedData
-                    detectTapGestures(
-                        onTap = { tapOffset ->
-                            hitboxRecs.value.forEachIndexed { index, rect ->
-                                if (rect.contains(tapOffset) && sortedDataLocal != null) {
-                                    selectedDataPoint = sortedDataLocal[index]
-                                    selectedDataPointIndex = index
-                                }
-                            }
-                        }
-                    )
-                }
-                .drawWithCache {
+        GraphContent(
+            sortedData = sortedData,
+            selectedDataPoint = selectedDataPoint,
+            onSelectedDataPointChanged = { newDataPoint ->
+                selectedDataPoint = newDataPoint
+            },
+            selectedDataPointIndex = selectedDataPointIndex,
+            onSelectedDataPointIndexChanged = { newIndex ->
+                selectedDataPointIndex = newIndex
+            },
+            isLoading = isLoading,
+            checked = checked
+        )
 
-                    Log.d("GraphScreen", "drawWithCache recomposed")
-
-                    var path: Path? = null
-                    boxSize = size
-
-                    if (dataChanged && sortedData != null) {
-                        maxPrice = sortedData
-                            .maxOf { it.nokPerKwh }
-                            .toFloat()
-                        minPrice = sortedData
-                            .minOf { it.nokPerKwh }
-                            .toFloat()
-
-                        val paddingPercentage = 0.25f
-                        val availableHeight = (1 - paddingPercentage) * size.height
-
-                        yScale = availableHeight / (maxPrice - minPrice)
-                        xScale = size.width / (sortedData.size - 1)
-
-                        scaleState = Pair(xScale, yScale)
-
-                        path = generatePath(sortedData, xScale, yScale, size)
-                    }
-
-                    onDrawBehind {
-                        drawGrid(size)
-
-                        if (dataChanged) {
-                            if (path != null) {
-                                drawPath(path, Color.Gray, style = Stroke(4.dp.toPx()))
-                                println("New path drawn?")
-                            }
-
-                            val paint = android.graphics
-                                .Paint()
-                                .apply {
-                                    color = android.graphics.Color.BLACK
-                                    textSize = 28f
-                                    textAlign = android.graphics.Paint.Align.CENTER
-                                }
-
-                            sortedData.forEachIndexed { i, priceData ->
-                                val x = i * xScale
-                                val y = size.height + 40
-
-                                val timeLabel = convertTime(priceData.timeStart, "HH")
-
-                                drawContext.canvas.nativeCanvas.drawText(
-                                    timeLabel,
-                                    x,
-                                    y,
-                                    paint
-                                )
-                            }
-
-                            hitboxRecs.value.clear()
-                            pointRecs.value.clear()
-
-                            val circleRadius = 4.dp.toPx()
-                            sortedData.forEachIndexed { i, priceData ->
-                                val x = i * xScale
-                                val y =
-                                    (size.height - (priceData.nokPerKwh - minPrice) * yScale - bottomOffset).toFloat()
-
-                                drawCircle(
-                                    Color.DarkGray,
-                                    radius = circleRadius,
-                                    center = Offset(x, y)
-                                )
-
-                                val pointRect = Rect(
-                                    top = y - circleRadius,
-                                    left = x - circleRadius,
-                                    bottom = y + circleRadius,
-                                    right = x + circleRadius
-                                )
-
-                                pointRecs.value.add(pointRect)
-
-                                val hitboxRect = Rect(
-                                    top = 0f,
-                                    left = x - (circleRadius + 9),
-                                    bottom = size.height,
-                                    right = x + (circleRadius + 9)
-                                )
-
-                                hitboxRecs.value.add(hitboxRect)
-                            }
-                        }
-                    }
-                }
-        ) {
-            if (shouldUpdateDataPoint && activeButton === "today" && sortedData !== null && selectedDataPoint === null) {
-                if ((selectedHour) < sortedData.size) {
-                    selectedDataPoint = sortedData[selectedHour]
-                    selectedDataPointIndex = selectedHour
-                } else {
-                    selectedDataPoint = null
-                }
-                shouldUpdateDataPoint = false
-            }
-
-            showTooltip =
-                !isLoading && selectedDataPoint !== null && selectedDataPointIndex < pointRecs.value.size
-
-            if (showTooltip) {
-                val currentRect = pointRecs.value[selectedDataPointIndex]
-                PriceTooltip(
-                    selectedDataPoint = selectedDataPoint,
-                    rect = currentRect,
-                    includeFees = checked
-                )
-            }
-        }
         Row(
             modifier = Modifier.padding(top = 15.dp),
             verticalAlignment = Alignment.CenterVertically
@@ -286,10 +136,10 @@ fun GraphScreen(navController: NavController) {
             )
         }
     }
-
 }
 
-private fun generatePath(data: List<PriceData>, xScale: Float, yScale: Float, size: Size): Path {
+
+fun generatePath(data: List<PriceData>, xScale: Float, yScale: Float, size: Size): Path {
 
     val path = Path()
     val minPrice = data.minOf { it.nokPerKwh }
@@ -309,6 +159,7 @@ private fun generatePath(data: List<PriceData>, xScale: Float, yScale: Float, si
 }
 
 fun DrawScope.drawGrid(size: Size, lines: Int = 4) {
+
     val barWidthPx = 1.dp.toPx()
     val color = Color.Gray
 
@@ -334,3 +185,4 @@ fun DrawScope.drawGrid(size: Size, lines: Int = 4) {
         )
     }
 }
+
