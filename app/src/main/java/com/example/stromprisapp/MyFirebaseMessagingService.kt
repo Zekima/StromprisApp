6c2222703e0e2175cb8c128d8c57b99a83826442
Kristian Veggeland <kristianov2000@hotmail.com>
2023-11-10 20:20:00 +0100

    Added some stuff that was forgotten in the merge.

--- app/src/main/java/com/example/stromprisapp/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/stromprisapp/MyFirebaseMessagingService.kt
@@ -1,5 +1,6 @@
 package com.example.stromprisapp
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -14,27 +15,45 @@ class MyFirebaseMessagingService : FirebaseMessagingService(){
         super.onNewToken(token)
     }
 
+    @SuppressLint("LaunchActivityFromNotification")
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+        println("Notification received")
+        super.onMessageReceived(message)
+        val a = message.data["action"]
+        if (a == "open_application") {
+            println(13)
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val id = 1
+            val channelId = "Nye Priser"
+            val channelName = message.messageType
+            manager.createNotificationChannel(NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH))
 
-        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val id = 1
-        val rCode = 1
-        val channelId = "Nye priser"
-        val channelName = "TestingName"
-        manager.createNotificationChannel(NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH))
+            val title = message.notification?.title ?: "Default Title"
+            val content = message.notification?.body ?: "Default Content"
 
-        val intent = Intent(this, MainActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(this,
-            rCode, intent, PendingIntent.FLAG_IMMUTABLE)
+            val intent = Intent(this, MainActivity::class.java)
 
-        val message = NotificationCompat.Builder(this, channelId)
-            .setContentTitle(message.notification?.title)
-            .setContentText(message.notification?.body)
-            .setAutoCancel(true)
-            .setContentIntent(pendingIntent)
-            .build()
-        manager.notify(id, message)
+            val pendingIntent = PendingIntent.getBroadcast(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_IMMUTABLE
+            )
+
+            // Build the notification
+            val builder = NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.power_icon)
+                .setContentTitle(title)
+                .setStyle(NotificationCompat.BigTextStyle().bigText(content))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentText("Praise Jesus!")
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+
+            // notificationId is a unique int for each notification that you must define
+            manager.notify(id, builder.build())
+
+        }
     }
-
 }
