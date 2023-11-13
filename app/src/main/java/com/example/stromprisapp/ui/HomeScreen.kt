b3a10abc7e21e2ac19b625e936651b95c0da61e7
Christian Alexander Ledaal <40630712+Zekima@users.noreply.github.com>
2023-11-13 10:04:45 +0100

    Fix import

--- app/src/main/java/com/example/stromprisapp/ui/HomeScreen.kt
+++ app/src/main/java/com/example/stromprisapp/ui/HomeScreen.kt
@@ -5,8 +5,10 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
